9061685f422347ab1c19ef16cb8ce3230f557f59
Lenny Truong <lenny@stanford.edu>
2021-03-29 13:44:45 -0700

    Add recursive prototype

--- session/atm_rec.py
+++ session/atm_rec.py
@@ -1,5 +1,6 @@
 from hwtypes import BitVector
 from session import Epsilon, Receive, Send, Offer, Choose, Dual, Rec, Channel
+from check import check
 
 
 ATMAuth = Rec("ATMAuth", Offer[
@@ -10,8 +11,7 @@ ATMAuth = Rec("ATMAuth", Offer[
 ])
 
 ATM = Receive[BitVector[32], Choose[("ok", ATMAuth), ("err", Epsilon)]]
-print(ATM)
-print(Dual(ATM))
+Client = Dual(ATM)
 
 
 def approved(id):
@@ -29,50 +29,51 @@ def set_balance(id, balance):
     balances[id] = balance
 
 
+@check
 def atm(c: Channel[ATM]):
     id = c.receive()
     if not approved(id):
-        c.right.close()
-        return
-    balance = get_balance(id)
-    c.left()
-    while True:
-        choice = c.offer()
-        if choice == "deposit":
-            amount = c.receive()
-            balance += amount
-            set_balance(id, balance)
-            c.send(balance)
-        elif choice == "withdraw":
-            amount = c.receive()
-            if balance >= amount:
-                balance -= amount
-                c.left()
-            else:
-                c.right().close()
-        elif choice == "exit":
-            c.close()
+        c.choose('err')
+        c.close()
+    else:
+        balance = get_balance(id)
+        c.choose('ok')
+        while True:
+            if c.offer("deposit"):
+                amount = c.receive()
+                balance += amount
+                set_balance(id, balance)
+                c.send(balance)
+            elif c.offer("withdraw"):
+                amount = c.receive()
+                if balance >= amount:
+                    balance -= amount
+                    c.choose('ok')
+                else:
+                    c.choose('err')
+                    c.close()
+                    return
+            elif c.offer("quit"):
+                c.close()
+                return
 
 
+@check
 def client(c: Channel[Client]):
     id = 2
     c.send(id)
-    choice = c.offer()
-    if choice == "ok":
+    if c.offer("ok"):
         c.choose("deposit")
         c.send(5)
-        choice = c.offer()
-        if choice == "ok":
-            print("Deposit succeeded")
-        elif choice == "err":
-            print("Insufficient funds")
+        balance = c.receive()
+        print(f"Deposit succeeded, balance={balance}")
         c.choose("withdraw")
         c.send(305)
-        choice = c.offer()
-        if choice == "ok":
+        if c.offer("ok"):
             print("Withdraw succeeded")
-        elif choice == "err":
+            c.choose("quit")
+        elif c.offer("err"):
             print("Insufficient funds")
-    elif choice == "err":
+    elif c.offer("err"):
         print("Invalid auth")
     c.close()
