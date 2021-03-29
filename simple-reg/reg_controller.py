7a3bcf1ee4e17661a0f32aec5edfced94a42fd0c
Lenny Truong <lenny@stanford.edu>
2021-03-29 14:49:36 -0700

    Syntax

--- simple-reg/reg_controller.py
+++ simple-reg/reg_controller.py
@@ -13,7 +13,7 @@ RegInit = m.Receive[Command.POWER_ON, m.Receive[Command.BOOT, m.Epsilon]]
 
 class RegController:
     def __init__(self):
-        self.io = m.IO(command=m.In(Channel[RegInit]), curr_sum=m.In(T),
+        self.io = m.IO(command=m.Channel[RegInit], curr_sum=m.In(T),
                        accum_input=m.In(T), accum_output=m.Out(T))
         self.accum = m.Register(T=T)
         self.X = X()
