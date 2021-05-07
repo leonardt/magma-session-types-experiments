c6c43e15bd990a100cd80087b2a31965115e3f70
Lenny Truong <lenny@stanford.edu>
2021-05-07 09:51:37 -0700

    Update controller

--- simple-reg/reg_controller.py
+++ simple-reg/reg_controller.py
@@ -1,40 +1,21 @@
-from typing import TypeVar
 import magma as m
-import hwtypes as ht
-from main import rand_value, X, T
-
-from controller import controller, Receive, Epsilon, Channel, Rec, Choose
 
 
-class Command(m.Enum):
-    POWER_ON = 0
-    BOOT = 1
-
-
-AccumRegAbstract = Receive[Command.POWER_ON,
-                           Receive[Command.BOOT, Epsilon]]
-HoldLow = Rec("HoldLow", Receive[0, "HoldLow"])
-PowerOnConcrete = Rec("PowerOnConcrete", Choose[(0, "PowerOnConcrete"),
-                                                (1, HoldLow)])
-T = m.Bits[8]
-BootConcrete = Rec("BootConcrete", Choose[((T, 0), "BootConcrete"),
-                                          ((T, 1), HoldLow)])
-
-
-@controller
+@m.coroutine()
 class RegController:
-    def __call__(self,
-                 abstract: Channel[AccumRegAbstract],
-                 power_on: Channel[PowerOnConcrete],
-                 boot: Channel[BootConcrete]):
+    def __init__(self):
+        pass
 
-        def wait_for_next_command():
-            while ~abstract.receive():
-                yield power_on.send(0), boot.send(0)
-
-        yield from wait_for_next_command()
-        yield power_on.send(1), boot.send(0)
-        yield from wait_for_next_command()
-        yield power_on.send(0), boot.send(1)
+    def __call__(self, advance: m.Bit) -> (m.Bit, m.Bit):
+        while ~advance:
+            yield m.bit(0), m.bit(0)
+        yield m.bit(1), m.bit(0)
+        while ~advance:
+            yield m.bit(0), m.bit(0)
+        yield m.bit(0), m.bit(1)
         while True:
-            yield power_on.send(0), boot.send(0)
+            yield m.bit(0), m.bit(0)
+
+
+if __name__ == "__main__":
+    m.compile("build/RegController", RegController, inline=True)
