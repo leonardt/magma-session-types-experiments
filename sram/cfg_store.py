3cedcee7eb6a4882b2005753a1022bd0a127a1c6
Lenny Truong <lenny@stanford.edu>
2021-09-10 10:28:47 -0700

    Add cfg store unit test

--- sram/cfg_store.py
+++ sram/cfg_store.py
@@ -26,10 +26,13 @@ class ConfigStore(m.Circuit):
         config_resp=m.Producer(m.ReadyValid[ConfigData]),
         boot=m.In(m.Bit)
     ) + m.ClockIO()
+    booted = m.Register(m.Bit)()
+    booted.I @= io.boot | booted.O
+
     mem = m.Memory(2 ** 8, m.Bits[8])()
     mem.RADDR @= io.config_req.data.addr
     read_ctrl_state = m.Register(READ_CTRL_STATE)()
-    io.config_req.ready @= read_ctrl_state.O == READ_CTRL_STATE.IDLE
+    io.config_req.ready @= read_ctrl_state.O == READ_CTRL_STATE.IDLE & booted.O
 
     is_read_cmd = ((io.config_req.data.cmd == CONFIG_CMD.READ) &
                    io.config_req.valid)
@@ -37,7 +40,7 @@ class ConfigStore(m.Circuit):
     read_ctrl_state.I @= m.mux(
         [
             # Transition to read state when valid read command
-            m.bits(io.config_req.valid & is_read_cmd, 1),
+            m.bits(io.config_req.valid & is_read_cmd & booted.O, 1),
             # Stay in read state until consumer is ready
             m.mux([READ_CTRL_STATE.READ, READ_CTRL_STATE.IDLE],
                   io.config_resp.ready)
@@ -52,9 +55,38 @@ class ConfigStore(m.Circuit):
     mem.WDATA @= io.config_req.data.data
     # TODO: We could add some delay here to make things interesting
     is_write_cmd = ((io.config_req.data.cmd == CONFIG_CMD.WRITE) &
-                    io.config_req.valid)
+                    io.config_req.valid & booted.O)
     mem.WE @= is_write_cmd
 
 
 if __name__ == "__main__":
-    m.compile("build/ConfigStore", ConfigStore)
+    import fault
+    tester = fault.Tester(ConfigStore, ConfigStore.CLK)
+    tester.circuit.config_req.ready.expect(0)
+    tester.step(2)
+    tester.circuit.boot = 1
+    tester.step(2)
+    tester.circuit.boot = 0
+    tester.circuit.config_req.ready.expect(1)
+    tester.step(2)
+    tester.circuit.config_req.data.addr = 0xDE
+    tester.circuit.config_req.data.data = 0xAD
+    tester.circuit.config_req.data.cmd = CONFIG_CMD.WRITE
+    tester.circuit.config_req.valid = 1
+    tester.step(2)
+
+    tester.circuit.config_req.data.addr = 0xDE
+    tester.circuit.config_req.data.cmd = CONFIG_CMD.READ
+    tester.circuit.config_req.valid = 1
+    tester.circuit.config_req.ready.expect(1)
+    tester.step(2)
+    # Should not be ready waiting for resp to be taken
+    tester.circuit.config_req.ready.expect(0)
+    tester.circuit.config_resp.valid.expect(1)
+    tester.circuit.config_resp.data.expect(0xAD)
+    tester.circuit.config_resp.ready = 1
+    tester.step(2)
+    tester.circuit.config_req.ready.expect(1)
+    tester.circuit.config_resp.valid.expect(0)
+
+    tester.compile_and_run("verilator")
